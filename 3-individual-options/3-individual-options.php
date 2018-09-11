4133c3781040ff6a9a816032771e0f32d92fbc7e
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-09-11 11:40:26 +0200

    update

--- 3-individual-options/3-individual-options.php
+++ 3-individual-options/3-individual-options.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  Plugin Name: Chapter 3 - Individual Options
+  Plugin Name: 3 - Individual Options
   Plugin URI: 
   Description: Companion to recipe 'Creating default user settings on plugin initialization'
   Author: Maarten von Kreijfelt
