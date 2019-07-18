3eb88108ff317e5356fd969900726e771971764d
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2019-07-18 11:53:21 +0200

    message

--- ch8-bug-tracker-v1-1 copy.php
+++ ch8-bug-tracker-v1-1 copy.php
@@ -3,9 +3,9 @@
   Plugin Name: Chapter 8 - Bug Tracker V1_1
   Plugin URI:
   Description: Companion to recipe 'Creating new database tables'
-  Author: Maarten von Kreijfelt
+  Author: Maarten von kreijfelt
   Version: 1.0
-  Author URI: 
+  Author URI:
  */
 
 // Register function to be called when plugin is activated
